4c9f05f2a5c0cd00723352cb4b7bb6d2b1e7c7c6
leep1000 <leepalmerformal@gmail.com>
2023-11-04 01:55:38 +0000

    25% done

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,4 +1,5 @@
-﻿Random dice = new Random();
+﻿/*
+Random dice = new Random();
 int roll1 = dice.Next();
 int roll2 = dice.Next(101);
 int roll3 = dice.Next(50, 101);
@@ -9,3 +10,10 @@ Console.WriteLine($"First roll: {roll3}");
 
 
 dice.Next();
+*/
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = System.Math.Max(secondValue, firstValue);
+
+Console.WriteLine(largerValue);
