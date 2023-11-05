fdd783643283e16e112298eee0c0bd01bf9d35ab
leep1000 <leepalmerformal@gmail.com>
2023-11-05 23:41:01 +0000

    dice game

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -10,10 +10,42 @@ Console.WriteLine($"First roll: {roll3}");
 
 
 dice.Next();
-*/
 
 int firstValue = 500;
 int secondValue = 600;
 int largerValue = System.Math.Max(secondValue, firstValue);
 
 Console.WriteLine(largerValue);
+*/
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1,7);
+int roll2 = dice.Next(1,7);
+int roll3 = dice.Next(1,7);
+
+int total = roll1 + roll2 + roll3; 
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    total += 2;
+    Console.WriteLine($"You rolled doubles! +2 bonus to total! New total: {total}");
+}
+
+if ((roll1 == roll2) && (roll2 == roll3))
+{
+    total += 6;
+    Console.WriteLine($"You rolled triples! +6 bonus to total! New total: {total}");
+}
+
+if (total > 14)
+{
+    Console.WriteLine("You win!");
+}
+
+if (total <= 14)
+{
+    Console.WriteLine("Sorry, you lose.");
+}
