a02de1e62980e04f01628a9e8a8dd82ff898b4cb
Palmer <lee.palmer@fujitsu.com>
2023-11-06 11:08:57 +0000

    intro to arrays

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -62,7 +62,6 @@ else
 {
   Console.WriteLine("You win a kitten!");  
 }
-*/
 
 Random random = new Random();
 int daysUntilExpiration = random.Next(12);
@@ -92,3 +91,30 @@ if (discountPercentage > 0)
 {
     Console.WriteLine($"Renew now and save {discountPercentage}%!");
 }
+
+string[] fraudulentOrderIDs = new string[3];
+
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+// fraudulentOrderIDs[3] = "D000";
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"First: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"First: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+string[] fraudulentOrderIDs = {"A123", "B456", "C789" };
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"First: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"First: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+*/
