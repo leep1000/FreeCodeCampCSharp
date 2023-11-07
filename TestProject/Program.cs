f57a9f905fdb4999774ce0bab623c6c8988ab9e6
Palmer <lee.palmer@fujitsu.com>
2023-11-07 16:01:06 +0000

    foreach challenge bugfix

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -142,4 +142,88 @@ foreach (string ID in orderIDs)
         Console.WriteLine(ID);
     }
 }
+
+string firstName = "Bob";
+int widgetsPurchased = 7;
+Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
+
+
+//   The following code creates five random OrderIDs
+//   to test the fraud detection process.  OrderIDs 
+//   consist of a letter from A to E, and a three
+//   digit number. Ex. A123.
+
+Random random = new Random();
+string[] orderIDs = new string[5];
+// Loop through each blank orderID
+for (int i=0; i < orderIDs.Length; i++)
+{
+    // Get a random value that equates to ASCII letters A through E
+    int prefixValue = random.Next(65, 70);
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    // Create a random number, pad with zeroes
+    string suffix = random.Next(1, 1000).ToString("000");
+    orderIDs[i] = prefix + suffix;
+}
+
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
+}
+
+// Example 1:
+Console
+.
+WriteLine
+(
+"Hello Example 1!"
+)
+;
+
+// Example 2:
+string firstWord="Hello";string lastWord="Example 2";Console.WriteLine(firstWord+" "+lastWord+"!");
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) 
+{
+    if ((roll1 == roll2) && (roll2 == roll3)) {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6; 
+    } else {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+}
+
 */
+
+// The high-level purpose of this code is to reverse a string and count the number of times a particular character appears.
+// Then prints the results
+
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+char[] reverseMessage = originalMessage.ToCharArray();
+Array.Reverse(reverseMessage);
+
+int count = 0;
+
+foreach (char letter in reverseMessage) 
+{ 
+    if (letter == 'o') 
+    { 
+        count++; 
+    } 
+}
+
+string newMessage = new String(reverseMessage);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {count} times.");
