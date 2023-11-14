02c88b8996c6c7502edcfc29734934c455ba5ab3
Palmer <lee.palmer@fujitsu.com>
2023-11-14 15:54:32 +0000

    completed initial for loop module

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -440,5 +440,62 @@ switch (product[2])
         break;
 }
 
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+    if (i == 7)
+    {
+        break;
+    }
+}
+
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = names.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(names[i]);
+}
+
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+foreach (var name in names)
+{
+    // Can't do this:
+    if (name == "David") name = "Sammy";
+}
+
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = 0; i < names.Length; i++)
+{
+    if (names[i] == "David")
+    {
+        names[i] = "Sammy";
+    }
+}
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
+
 */
 
+// FizzBuzz Challenge
+
+for (int i = 1; i <= 100; i++)
+{
+    if ((i % 3 == 0) && (i % 5 == 0))
+    {
+        Console.WriteLine($"{i} - FizzBuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine($"{i} - Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine($"{i} - Buzz");
+    }
+    else
+    {
+        Console.WriteLine($"{i}");
+    }
+}
