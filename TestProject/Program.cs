6ab82b15c7405b3509b2c347766dbc7d44427cdf
Palmer <lee.palmer@fujitsu.com>
2023-11-16 22:30:44 +0000

    learned about while loops

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -476,8 +476,6 @@ foreach (var name in names)
     Console.WriteLine(name);
 }
 
-*/
-
 // FizzBuzz Challenge
 
 for (int i = 1; i <= 100; i++)
@@ -499,3 +497,27 @@ for (int i = 1; i <= 100; i++)
         Console.WriteLine($"{i}");
     }
 }
+
+*/
+
+Random random = new Random();
+int current = random.Next(1, 11);
+
+do
+{
+    current = random.Next(1, 11);
+
+    if (current >= 8)
+    {
+        continue;
+    }
+
+    Console.WriteLine(current);
+} while (current != 7);
+
+// while (current >= 3)
+// {
+//     Console.WriteLine(current);
+//     current = random.Next(1, 11);
+// }
+// Console.WriteLine($"Last number: {current}");
